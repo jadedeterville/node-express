81f19a5111d0066cd5eb252213edaee1596c2a47
jadedeterville <jadedeterville@gmail.com>
2022-06-18 11:09:17 +0100

    Intro to Express Part 2

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 // states that we are using express, its not a core module but we dont need to give it a file path
 const express = require('express');
 
+const morgan = require('morgan');
+
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
+app.use(morgan('dev'));
+
+app.use(express.static(__dirname + '/public'));
 
 app.use((req, res) => {
-    console.log(req.headers);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is an Express Server</h1></body></html>');
