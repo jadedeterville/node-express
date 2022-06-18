04d0b04b0a84fa3632c21f1fc50b4b639447b62d
jadedeterville <jadedeterville@gmail.com>
2022-06-18 11:36:42 +0100

    Express Simple REST

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
-// states that we are using express, its not a core module but we dont need to give it a file path
 const express = require('express');
-
 const morgan = require('morgan');
 
 const hostname = 'localhost';
@@ -8,6 +6,56 @@ const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));
+app.use(express.json());
+
+// catch all for http verbs 
+app.all('/campsites', (req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+    // next passes control of the routing to the next relevant routing method
+});
+// above is a callback function with args of req and res  
+
+// the next routing method below 
+app.get('/campsites', (req, res) => {
+    res.end('Will send all the campsites to you');
+});
+
+// posts carries info typically in json 
+app.post('/campsites', (req, res) => {
+    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
+});
+
+// 403 operation not supported 
+app.put('/campsites', (req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /campsites');
+});
+
+// delete is a dangerous opp - need to restrict to only users with certain privileges 
+app.delete('/campsites', (req, res) => {
+    res.end('Deleting all campsites');
+});
+
+app.get('/campsites/:campsiteId', (req, res) => {
+    res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
+});
+
+app.post('/campsites/:campsiteId', (req, res) => {
+    res.statusCode = 403;
+    res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
+});
+
+app.put('/campsites/:campsiteId', (req, res) => {
+    res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
+    res.end(`Will update the campsite: ${req.body.name}
+        with description: ${req.body.description}`);
+});
+
+app.delete('/campsites/:campsiteId', (req, res) => {
+    res.end(`Deleting campsite: ${req.params.campsiteId}`);
+});
 
 app.use(express.static(__dirname + '/public'));
 
@@ -17,8 +65,6 @@ app.use((req, res) => {
     res.end('<html><body><h1>This is an Express Server</h1></body></html>');
 });
 
-
-// creates an instance of the http server class and listens to it
 app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
