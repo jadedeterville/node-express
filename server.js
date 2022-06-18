01fb256881a4238a790cdd9db4546fe42c474244
jadedeterville <jadedeterville@gmail.com>
2022-06-18 11:02:24 +0100

    Intro to Express Part 1

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// states that we are using express, its not a core module but we dont need to give it a file path
 const express = require('express');
 
 const hostname = 'localhost';
@@ -12,6 +13,8 @@ app.use((req, res) => {
     res.end('<html><body><h1>This is an Express Server</h1></body></html>');
 });
 
+
+// creates an instance of the http server class and listens to it
 app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
